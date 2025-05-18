2155c220e91c816f905b5642774967ace15611c1
Mohammadamin Rashidi <bamdadrshydy@gmail.com>
2025-05-18 10:15:33 -0700

    useless class btw

--- connections.java
+++ connections.java
@@ -4,5 +4,7 @@ import java.net.Socket;
 
 public class connections {
 
-
+    // useless as shit lmao
+    // yall don't need this class to get the program to work
+    // my old silly self though i can put all classes in one big file but that is bad practice 👌🗿
 }
